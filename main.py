88ea6950084f7e80bc5d98fd875d808a753edf3e
João Vitor Barbosa <j.vs.b13@gmail.com>
2025-01-20 00:34:38 -0300

    Separa tabela DimTempo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import numpy as np
 from pyspark.sql import SparkSession
 
@@ -77,6 +78,31 @@ def separar_justificativas(path: str):
             saida.write(f"{_id},{line}\n")
 
 
+def separar_tempo(path: str):
+    #TODO: Talvez remover os zeros a esquerda de mes, dia, hora e minuto
+
+    linhas_to_write: set[tuple[str, ...]] = set()
+
+    def _separar_data_iso(data: str): return data[:4], data[5:7], data[8:10]
+
+    def _separar_hora_iso(tempo: str): return tempo[:2], tempo[3:5]
+
+    with open(path, encoding='latin1') as entrada, open('BrFlights2/DimTempo.csv', 'w', encoding='latin-1') as saida:
+        next(entrada) # ignora cabecalho
+        for line in entrada:
+            line_list = line.strip().split(',')
+            partida_data, partida_hora = line_list[ColunasDataset.PARTIDA_PREVISTA].strip('Z').split('T')
+            linhas_to_write.add(_separar_data_iso(partida_data) + _separar_hora_iso(partida_hora))
+
+            if not line_list[ColunasDataset.PARTIDA_REAL] == 'NA':
+                partida_data, partida_hora = line_list[ColunasDataset.PARTIDA_REAL].strip('Z').split('T')
+                linhas_to_write.add(_separar_data_iso(partida_data) + _separar_hora_iso(partida_hora))
+
+        saida.write(f"id,ano,mes,dia,hora,minuto\n")
+        for _id, line in enumerate(sorted(linhas_to_write), start=1):
+            saida.write(f"{_id},{','.join(line)}\n")
+
+
 def separar_voos(path: str):
     aeronaves = map_aeronave_to_id()
     aeroportos = map_aeroporto_to_id()
@@ -138,7 +164,8 @@ def map_aeroporto_to_id(path='./BrFlights2/aeroportos.csv') -> dict[tuple[float,
 def separar_tabelas(dataset_path: str):
     # separar_companhias(dataset_path)
     # separar_aeroportos(dataset_path)
-    separar_justificativas(dataset_path)
+    # separar_justificativas(dataset_path)
+    separar_tempo(dataset_path)
     # separar_voos(dataset_path)
 
 
